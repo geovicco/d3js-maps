cc24a8489a32e2353561a044a3840eaf55ec68f8
geovicco <71774828+geovicco@users.noreply.github.com>
2020-11-18 01:13:33 +0530

    Change Canvas Size and Add Text
    Resized the Canvas and added text to the map that displays the name of the state when hovering a region.

--- 1_basic-geojson-map/script.js
+++ 1_basic-geojson-map/script.js
@@ -5,7 +5,7 @@ let statesData
 
 let canvas = d3.select('#canvas')
 
-var projection = d3.geoMercator().scale(1050).center([80, 24]) // Scale is used to zoom into the features and center is used to set the center lat lon on the canvas
+var projection = d3.geoMercator().scale(1220).center([78, 26]) // Scale is used to zoom into the features and center is used to set the center lat lon on the canvas
 
 let drawMap = () => {
     canvas.selectAll('path')
@@ -15,9 +15,14 @@ let drawMap = () => {
             .attr('d', d3.geoPath().projection(projection)) // Change the default projection
             .attr('class', 'state')
             .attr("fill", "grey") // the color of the state polygon
+            .style("opacity", 0.75)
             .style("stroke", "#fff") // the color of state borders
-            .style("stroke-width", "0.1px") // The thickness of state borders
-
+            .style("stroke-width", "0.2px") // The thickness of state borders
+            // Get text to show up when hovering over a particular state
+            .append('title')
+            .text((function(d) {
+                return d.properties.NAME_1 // Displays the State Name when mouse is hovered over a region of the map
+            }))
 }
 
 // Fetch Data from Topojson File
@@ -31,7 +36,7 @@ d3.json(ind_states_source).then(
             // Convert TopoJSON features to GeoJSON Features
         
             statesData = topojson.feature(data, data.objects.IND_adm1).features
-            // console.log(statesData)
+            console.log(statesData)
 
             // Initialize Draw Map Function to display the map 
             drawMap()
